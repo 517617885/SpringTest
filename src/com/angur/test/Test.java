50702a70f4ca20f7110a3ae51cc8f1e0e8fa7ee9
Angur <pmwang@yeah.net>
2017-06-17 17:17:35 +0800

    Change-Id: I0d018fffc092b8ac237af92397d2807b370cd2f3 Signed-off-by: Angur <pmwang@yeah.net>

--- src/com/angur/test/Test.java
+++ src/com/angur/test/Test.java
@@ -1,7 +1,10 @@
 package com.angur.test;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 /** 
 * @description 描述：
@@ -14,10 +17,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 */
 public class Test {
 
-	public static void main(String[] args) {
+	@org.junit.Test
+	public void testClassPathInfo() {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 		Person person = context.getBean("person", Person.class);
+		person.info();
+	}
+	
+	@org.junit.Test
+	public void testSimpleLoad(){
+		
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("bean.xml"));
+		Person person = (Person) bf.getBean("person");
 		person.info();
 	}
 }
